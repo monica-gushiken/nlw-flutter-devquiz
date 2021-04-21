387b20bf825d2516b81ff68e6cedae5928f422df
Monica Mayumi Gushiken <monica-gushiken@users.noreply.github.com>
2021-04-21 12:12:40 -0300

    aula 03 - In orbit - estruturação dos dados do app

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,12 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:devquiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
@@ -25,7 +27,7 @@ class AppBarWidget extends PreferredSize {
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
-                              text: 'Monica',
+                              text: user.name,
                               style: AppTextStyles.titleBold,
                             ),
                           ],
@@ -38,7 +40,7 @@ class AppBarWidget extends PreferredSize {
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
                             image: NetworkImage(
-                              'https://avatars.githubusercontent.com/u/2030471?v=4',
+                              user.photoUrl,
                             ),
                           ),
                         ),
